bd808c7ec6923870682097ca7c6237b2c0c20727
Marco Ramirez Davila <mramirezd@vc.com.mx>
2020-08-19 16:36:33 -0500

    Se agregan validaciones

--- utils-doc/utils-headers.js
+++ utils-doc/utils-headers.js
@@ -44,9 +44,11 @@ function getHeader(document, doc) {
             children.push(utilsDocx.addParagraph({ text: ' ', children: [table] }));
         });
     }
-    if (document.head.img) {
-        const img = utilsDocx.defaultImg(doc, document.head.img._text);
-        children.push(utilsDocx.addParagraph({ children: [img] }));
+    if (document) {
+        if (document.head.img) {
+            const img = utilsDocx.defaultImg(doc, document.head.img._text);
+            children.push(utilsDocx.addParagraph({ children: [img] }));
+        }
     }
     const header = {
         default: utilsDocx.addHeader({
